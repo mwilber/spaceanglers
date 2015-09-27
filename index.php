d94440c717c0c391f6eaa9bb6f3a2435dc893679
Matt Wilber <mwilber@gmail.com>
2015-09-26 22:40:29 -0400

    add fontawesome

--- index.php
+++ index.php
@@ -37,6 +37,7 @@
 
 	<link href='http://fonts.googleapis.com/css?family=Press+Start+2P' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/jquery.facebook.multifriend.select.css" />
 	<link rel='stylesheet' id='camera-css'  href='css/flexslider.css' type='text/css' media='all'>
